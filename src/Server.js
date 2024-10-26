36ddd615adfdea094cc9d1044d60efa5a0dd6490
Muhammed TERZİ <muhammet.terzi99@gmail.com>
2024-10-26 14:11:10 +0300

    Mesaj Gönderimi
    text,poll ve media türünde mesajların başarılı bir şekilde gönderilmesi tamamlandı.

--- src/Server.js
+++ src/Server.js
@@ -13,6 +13,123 @@ app.get('/', (req, res) => {
     res.send('Merhaba dünya! Şu anda test sunucusu çalışıyor.');
 });
 
+// Text Message
+app.post('/mesaj/text', async (req, res) => {
+    // toNumber, message
+    try {
+        const toNumber = req.body.toNumber;
+        const message = req.body.message;
+
+        const response = await axios.post(
+            `https://api.maytapi.com/api/${ MAYTAPI_PRODUCTID }/${ MAYTAPI_PHONEID }/sendMessage`,
+            {
+                "type": "text",
+                "to_number": toNumber, 
+                "message": message
+            },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    "x-maytapi-key": MAYTAPI_TOKEN
+                }
+            }
+        );
+
+        console.log(response.data)
+        if(!response.data.success) {
+            res.status(404).send(response.data.message);
+        }
+        else {
+            //  Veritabanı işlemleri buraya gelecek.
+            // response.data.data.chatId, response.data.data.msgId
+            res.status(200).send('Mesaj basariyla gonderildi!');
+        }
+    }
+    catch(err) {
+        res.status(404).send(err.message);
+    }
+});
+
+// Poll Message
+
+app.post('/mesaj/poll', async (req, res) => {
+    // toNumber, message, options, only_one (optional)
+    try {
+        const toNumber = req.body.toNumber;
+        const message = req.body.message;
+        const options = req.body.options;
+        const only_one = req.body.only_one;
+
+        const response = await axios.post(
+            `https://api.maytapi.com/api/${ MAYTAPI_PRODUCTID }/${ MAYTAPI_PHONEID }/sendMessage`,
+            {
+                "type": "poll",
+                "to_number": toNumber, 
+                "message": message,
+                "options": options,
+                "only_one": only_one
+            },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    "x-maytapi-key": MAYTAPI_TOKEN
+                }
+            }
+        );
+
+        console.log(response.data)
+        if(!response.data.success) {
+            res.status(404).send(response.data.message);
+        }
+        else {
+            //  Veritabanı işlemleri buraya gelecek.
+            // response.data.data.chatId, response.data.data.msgId
+            res.status(200).send('Mesaj basariyla gonderildi!');
+        }
+    }
+    catch(err) {
+        res.status(404).send(err.message);
+    }
+});
+
+app.post('/mesaj/media', async (req, res) => {
+    // toNumber, message, text
+    try {
+        const toNumber = req.body.toNumber;
+        const message = req.body.message;
+        const text = req.body.text;
+
+        const response = await axios.post(
+            `https://api.maytapi.com/api/${ MAYTAPI_PRODUCTID }/${ MAYTAPI_PHONEID }/sendMessage`,
+            {
+                "type": "media",
+                "to_number": toNumber, 
+                "message": message,
+                "text": text
+            },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    "x-maytapi-key": MAYTAPI_TOKEN
+                }
+            }
+        );
+
+        console.log(response.data)
+        if(!response.data.success) {
+            res.status(404).send(response.data.message);
+        }
+        else {
+            //  Veritabanı işlemleri buraya gelecek.
+            // response.data.data.chatId, response.data.data.msgId
+            res.status(200).send('Mesaj basariyla gonderildi!');
+        }
+    }
+    catch(err) {
+        res.status(404).send(err.message);
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Test uygulamam ${ PORT } portundan çalışıyor.`)
 });
